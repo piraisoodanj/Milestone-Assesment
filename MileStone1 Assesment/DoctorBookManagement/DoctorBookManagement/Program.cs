87205053dc274405a95c9cbe2c9b04ea1c232d5b
piraisoodanj <soodan1998@gmail.com>
2024-08-21 23:34:05 +0530

    Milestone 1 Assesment commit

---
+++ MileStone1 Assesment/DoctorBookManagement/DoctorBookManagement/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using DoctorsBook;
+
+namespace DoctorBookManagement
+{
+    internal class Program
+    {
+        // List to store collection of Doctor Details
+        static List<Doctor> DoctorsDetials = new List<Doctor>();
+
+        static void Main()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Doctor Management System");
+                Console.WriteLine("1. Add Doctor Information");
+                Console.WriteLine("2. Display Doctor's List");
+                Console.WriteLine("3. Exit");
+                Console.Write("Select an option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        AddDoctors();
+                        break;
+                    case "2":
+                        DisplayDoctors();
+                        break;
+                    case "3":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+        //Add Doctor details
+        static void AddDoctors()
+        {
+            try
+            {
+                Doctor newDoctor = new Doctor();
+                newDoctor.AcceptDoctorInformation();
+                DoctorsDetials.Add(newDoctor);
+                Console.WriteLine("Doctor added successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        //Display Doctors List
+        static void DisplayDoctors()
+        {
+            if (DoctorsDetials.Count == 0)
+            {
+                Console.WriteLine("No Doctors available.");
+            }
+           else
+            {
+                foreach (var Doc in DoctorsDetials)
+                {
+                    
+                    Console.WriteLine($"Registration No: {Doc.RegistrationNo}");
+                    Console.WriteLine($"Doctor Name: {Doc.DoctorName}");
+                    Console.WriteLine($"City: {Doc.City}");
+                    Console.WriteLine($"AreaofSpecialization: {Doc.AreaOfSpecialization}");
+                    Console.WriteLine($"ClinicAddress: {Doc.ClinicAddress}");
+                    Console.WriteLine($"ClinicTimings: {Doc.ClinicTimings}");
+                    Console.WriteLine($"Contact No: {Doc.ContactNo}");
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MileStone1 Assesment/DoctorsBook/DoctorsBook/Doctor.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DoctorsBook
+{
+    public class Doctor
+    {
+        //Assigning Variables
+        public string RegistrationNo { get; set; }
+        public string DoctorName {  get; set; }
+        public string City { get; set; }
+        public string AreaOfSpecialization { get; set; }
+        public string ClinicAddress { get; set; }
+        public string ClinicTimings { get; set; }
+        public string ContactNo { get; set; }
+      
+        // Method for Accept details from User
+        public void AcceptDoctorInformation()
+        {
+            Console.WriteLine("Enter Registration No");
+            RegistrationNo = Console.ReadLine();
+            ValidRegNo(RegistrationNo);
+
+            Console.WriteLine("Enter Doctor Name");
+            DoctorName = Console.ReadLine();
+            OnlyAlphabet(DoctorName);
+
+            Console.WriteLine("Enter City");
+            City = Console.ReadLine();
+            MandatoryField(City);
+
+            Console.WriteLine("Enter Area of Specialization");
+            AreaOfSpecialization = Console.ReadLine();
+            OnlyAlphabet(AreaOfSpecialization);
+
+            Console.WriteLine("Enter Clinic Address");
+            ClinicAddress = Console.ReadLine();
+            MandatoryField(ClinicAddress);
+
+            Console.WriteLine("Enter Clinic Timings");
+            ClinicTimings = Console.ReadLine();
+            MandatoryField(ClinicTimings);
+
+            Console.WriteLine("Enter Contact No");
+            ContactNo = Console.ReadLine();
+            ValidContactNo(ContactNo);
+
+        }
+
+        //Method for Valid Registration Number should be only 7 digit numbers
+        public void ValidRegNo(string RegNo)
+        {
+            string DigitPattern = @"^\d{7}$";
+
+            bool isMatch = Regex.IsMatch(RegNo, DigitPattern);
+            Console.WriteLine(isMatch);
+            Console.WriteLine(!String.IsNullOrEmpty(RegNo));
+            if (isMatch && !string.IsNullOrEmpty(RegNo))
+                return;
+            else
+               throw new ArgumentException("Registration No Should be of 7 digit only");
+
+        }
+
+        // Method for Valid Doctor Name and Area of Specialization Accept only alphabets
+
+        public void OnlyAlphabet(string Text)
+        {
+            string Textpatern = @"^[a-zA-Z]+$";
+
+            bool isTextMatch = Regex.IsMatch(Text, Textpatern);
+            if (isTextMatch && !string.IsNullOrEmpty(Text))
+                return;
+            else
+                throw new ArgumentException("Text Should be only aphabets");
+        }
+
+        // Method for Valid Contact Number
+        public void ValidContactNo(string ContactNumber)
+        {
+            string ContactNoPattern = @"^\d{10}$";
+
+            bool isMatch = Regex.IsMatch(ContactNumber, ContactNoPattern);
+            if (isMatch && !string.IsNullOrEmpty(ContactNumber))
+            {
+                return;
+            }
+            else
+            {
+
+                throw new ArgumentException("Contact Number Should be of 10 digit only");
+            }
+        }
+
+        public void MandatoryField(string MandatoryText)
+        {
+            if (!string.IsNullOrEmpty(MandatoryText))
+            {
+                return;
+            }
+            else
+            {
+                throw new ArgumentException("Mandatory Fileds are missing. Should not be empty");
+            }
+        }
+    }
+    
+}
+
