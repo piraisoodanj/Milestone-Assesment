c7d436d35a2b5b0e908510411f72e9e984031d51
piraisoodanj <soodan1998@gmail.com>
2024-10-15 11:04:50 +0530

    milestone 5
    commit

---
+++ MilestoneAssesment5/DatabaseAssesment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace DatabaseAssesment
+{
+    internal class Program
+    {
+
+        public static SqlConnection CreateConnection()
+        {
+            Console.WriteLine("Creating db connection.............");
+            SqlConnection con = new
+                SqlConnection("Data Source=62212DCB81D85A7;Initial Catalog=SampleDB;Integrated Security=True;");
+            Console.WriteLine("DB Connection successfull.");
+            return con;
+        }
+
+        public static void GetData(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("select * from Users", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["id"]+" | " + rdr["Name"].ToString().Trim()+" | "+ rdr["Email"]);
+            }
+            con.Close();
+        }
+
+     
+        static void Main(string[] args)
+        {
+            SqlConnection conn = CreateConnection();
+            Console.WriteLine("Loading Data..........");
+            Console.WriteLine("Displaying loaded Data.");
+            GetData(conn);
+        }
+    }
+}
---
+++ MilestoneAssesment5/SeleniumLoginPageAssesment/Program.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumLoginPageAssesment
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+
+
+            IWebElement UsernameField = driver.FindElement(By.Id("username"));
+            IWebElement PasswordField = driver.FindElement(By.Id("password"));
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var script = "document.getElementById('submit').click();";   //JQuery
+            
+
+
+
+            EnterTextBox(UsernameField, "student");  //Username fill
+            EnterTextBox(PasswordField, "Password123"); //Password Fill
+            js.ExecuteScript(script);  //click login button
+
+
+
+            IWebElement LoginSuccessPopup = driver.FindElement(By.XPath("//h1[@class='post-title']"));  //using XPath
+            CheckLoginSuccessPopup(LoginSuccessPopup);
+
+
+
+        }
+
+        public static void EnterTextBox(IWebElement webElement, string textVal)
+        {
+            webElement.SendKeys(textVal);
+        }
+
+        public static void CheckLoginSuccessPopup(IWebElement successPopup)
+        {
+            var LoginSuccessText = successPopup.Text;
+            if (LoginSuccessText == "Logged In Successfully")
+            {
+                Console.WriteLine("Successfully Logged In");
+            }
+            else
+            {
+                Console.WriteLine("Logg In Failed");
+            }
+        }
+    }
+}
